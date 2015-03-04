c3982693820c4bb0d4b49491236a94c91181205d
nayyer_j <nayyer_j@epitech.eu>
2015-03-04 17:31:45 +0100

    probleme sur passage map

--- lemipc.h
+++ lemipc.h
@@ -5,16 +5,39 @@
 ** Login   <nayyer_j@epitech.net>
 ** 
 ** Started on  Wed Mar  4 15:26:42 2015 Jhanzeeb Nayyer
-** Last update Wed Mar  4 15:28:50 2015 Jhanzeeb Nayyer
+** Last update Wed Mar  4 17:07:13 2015 Jhanzeeb Nayyer
 */
 
 #ifndef LEMIPC_H_
 # define LEMIPC_H_
+
+# include <sys/types.h>
+# include <sys/ipc.h>
+
+# define MAPMAX_SIZE (400)
+# define PATH_SIZE (1024)
 
 typedef enum	e_bool
   {
     FALSE,
     TRUE
   }		t_bool;
+
+typedef enum	e_state
+  {
+    EMPTY,
+    FIRST,
+    SECOND,
+    THIRD
+  }		t_state;
+
+typedef struct	s_settings
+{
+  int		shm_id;
+  int		sem_id;
+  int		msg_id;
+  void		*addr;
+  key_t		key;
+}		t_settings;
 
 #endif /* !LEMIPC_H_ */
--- main.c
+++ main.c
@@ -5,50 +5,88 @@
 ** Login   <tavukc_k@epitech.net>
 **
 ** Started on  Wed Mar  4 11:13:37 2015 kevin tavukciyan
-** Last update Wed Mar  4 15:54:25 2015 Jhanzeeb Nayyer
+** Last update Wed Mar  4 17:29:15 2015 Jhanzeeb Nayyer
 */
 
-#include <sys/types.h>
 #include <sys/shm.h>
-#include <sys/ipc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "lemipc.h"
 
-# define BUFF_SIZE (1024)
-
-t_bool		initMemory()
+t_bool		initKey(t_settings *set)
 {
-  /* int		shm_id; */
-  char		path[BUFF_SIZE];
-  /* void		*addr; */
+  char		path[PATH_SIZE];
   key_t		key;
 
-  printf("#1 hello\n");
   if (getcwd(path, sizeof(path)) == NULL)
-    return (FALSE);
-  printf("#2 hello\n");
+    {
+      printf("Can not find current directory pathname\n");
+      return (FALSE);
+    }
   if ((key = ftok(path, 0)) != -1)
     printf("Key = [%d]\n", key);
   else
     {
-      printf("Mauvais pathname\n");
+      printf("Error init Key\n");
       return (FALSE);
     }
+  set->key = key;
+  return (TRUE);
+}
+
+t_bool		initMemory(t_settings *set)
+{
+  if ((set->shm_id = shmget(set->key, MAPMAX_SIZE, SHM_R | SHM_W)) == -1)
+    {
+      if ((set->shm_id = shmget(set->key, MAPMAX_SIZE,
+				IPC_CREAT | SHM_R | SHM_W)) == -1)
+        return (FALSE);
+      printf("Creating shm [%d]\n", set->shm_id);
+      if ((set->addr = shmat(set->shm_id, NULL, SHM_R | SHM_W)) == (void *) -1)
+	return (FALSE);
+      printf("in start -> %p\n", set->addr);
+      /* sprintf((char *)addr, "Epitech Paris rox"); */
+    }
+  else
+    {
+      printf("Already created [%d]\n", set->shm_id);
+      /* addr = shmat(set->shm_id, NULL, SHM_R | SHM_W); //vérification fonction */
+      /* printf("--> %s\n", (char *)addr); */
+      shmctl(set->shm_id, IPC_RMID, NULL); //vérification fonction
+    }
+  return (TRUE);
+}
+
+t_bool		initMap(t_settings *set)
+{
+  int		map[10][10];
+
+  memset(map, 0, sizeof(map)); //vérifier fonction
+  set->addr = (void *)map;
   return (TRUE);
 }
 
 t_bool		start()
 {
-  /* if (initMap() == FALSE) */
-  /*   return (FALSE); */
-  if (initMemory() == FALSE)
+  t_settings	*set;
+
+ if ((set = malloc(sizeof(t_settings))) == NULL)
     return (FALSE);
+  if (initKey(set) == FALSE)
+    return (FALSE);
+  if (initMemory(set) == FALSE)
+    return (FALSE);
+  if (initMap(set) == FALSE)
+    return (FALSE);
+  printf("%d hoho\n", map[0][0]);
+  printf("in start -> %p\n", set->addr);
   /* if (initSem() == FALSE) */
   /*   return (FALSE); */
   /* if (initMsg() == FALSE) */
   /*   return (FALSE); */
+  free(set);
   return (TRUE);
 }
 
